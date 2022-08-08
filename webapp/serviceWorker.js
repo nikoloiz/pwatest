6248f92ef7fddf1723eefff6729073930867d8c5
Niko <100766074+nikoloiz@users.noreply.github.com>
2022-08-08 09:13:32 +0100

    Add files via upload

---
+++ webapp/serviceWorker.js
@@ -0,0 +1,16 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
